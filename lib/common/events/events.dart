e55e7ecaa12637614f695fafbfaaf8b240b8955a
Naman <namanpreet@gmail.com>
2025-12-22 14:45:09 +0530

    Update events component

--- lib/common/events/events.dart
+++ lib/common/events/events.dart
@@ -15,6 +15,7 @@ import 'package:diohub/common/wrappers/infinite_scroll_wrapper.dart';
 import 'package:diohub/models/commits/commit_card_data_model.dart';
 import 'package:diohub/models/events/events_model.dart' hide Key, State;
 import 'package:diohub/models/issues/issue_card_data_model.dart';
+import 'package:diohub/models/issues/issue_model.dart';
 import 'package:diohub/providers/users/current_user_provider.dart';
 import 'package:diohub/services/activity/events_service.dart';
 import 'package:diohub/utils/utils.dart';
