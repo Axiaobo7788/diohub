ed952a4b989273c1544d28b8cc3f9a0130b6f46a
Naman <namanpreet@gmail.com>
2025-12-22 19:04:33 +0530

    Update shimmer and timeline components

--- lib/common/timeline/timeline_container.dart
+++ lib/common/timeline/timeline_container.dart
@@ -1,3 +1,4 @@
+import 'package:diohub/style/surface_style_theme.dart';
 import 'package:flutter/material.dart';
 
 /// Reusable container widget for timeline content items
@@ -16,7 +17,9 @@ class TimelineContainer extends StatelessWidget {
       padding: const EdgeInsets.all(12),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: Theme.of(context).surfaceStyle.borderRadius(
+          size: BorderRadiusSize.small,
+        ),
         border: Border.all(
           color: Theme.of(context).colorScheme.outlineVariant.withOpacity(0.3),
           width: 1,
